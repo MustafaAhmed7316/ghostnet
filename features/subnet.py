8ef980c805939d3aedb660fbc350b36cc66eadbb
snitchbossdotcom <snitchbossdotcom@gmail.com>
2026-05-13 13:39:13 +0500

    patches

--- features/subnet.py
+++ features/subnet.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 def subnet_scan(target, port=None):
-    binary = os.path.expanduser('subnetscanner/subnetrust/target/release/subnetrust')
+    binary = os.path.expanduser('features/subnetscanner/subnetrust/target/release/subnetrust')
     cmd = [binary, target]
     if port:
         cmd.append(str(port))
